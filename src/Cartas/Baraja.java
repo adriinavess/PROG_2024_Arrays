236359e157a7124f863f7c6914c0e0859178b8cb
adrii <adrii@LAPTOP-NP7PFLPP>
2024-01-22 10:14:55 +0100

    Resolucion tener todas las cartas

--- src/Cartas/Baraja.java
+++ src/Cartas/Baraja.java
@@ -8,8 +8,43 @@ public class Baraja {
 	
 	
 	public Baraja() {
-		
 		cartas = new Carta[52];
+		
+		for (int i = 0; i < 13; i++) {
+			cartas[i] = crearCartas(i+1, 'C');
+		}
+		for (int i = 0; i < 13; i++) {
+			cartas[13+i] = crearCartas(i+1, 'P');
+		}
+		for (int i = 0; i < 13; i++) {
+			cartas[26+i] = crearCartas(i+1, 'D');
+		}
+		for (int i = 0; i < 13; i++) {
+			cartas[39+i] = crearCartas(i+1, 'T');
+		}
+	}
+	
+	private Carta crearCartas(int numero, char palo) {
+		Carta carta = new Carta();
+		
+		switch(numero) {
+		case 1: carta.setValor('A'); break;
+		case 2: carta.setValor('2'); break;
+		case 3: carta.setValor('3'); break;
+		case 4: carta.setValor('4'); break;
+		case 5: carta.setValor('5'); break;
+		case 6: carta.setValor('6'); break;
+		case 7: carta.setValor('7'); break;
+		case 8: carta.setValor('8'); break;
+		case 9: carta.setValor('9'); break;
+		case 10: carta.setValor('D'); break;
+		case 11: carta.setValor('R'); break;
+		case 12: carta.setValor('Q'); break;
+		case 13: carta.setValor('K'); break;
+		}
+		carta.setPalo(palo);
+		
+		return carta;
 	}
 	
 	public String toString() {
